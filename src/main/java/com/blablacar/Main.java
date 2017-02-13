fce581ffd71bc30964335995422cbb3de37709a2
Milio Peralta <milio.peralta@gmail.com>
2017-02-13 23:30:25 +0100

    [UPDATE] main with single function, file path can be an arg

--- src/main/java/com/blablacar/Main.java
+++ src/main/java/com/blablacar/Main.java
@@ -5,7 +5,20 @@ import main.java.com.blablacar.business.Lawn;
 public class Main {
 
     public static void main(String[] args) {
-        Lawn lawn = new Lawn("C:\\Users\\Milio\\IdeaProjects\\mowers\\src\\main\\resources\\input.txt");
+        run(args);
+    }
+
+    private static void run(String[] args) {
+        String path;
+        if (args != null && args.length > 0 && args[0] != null && !args[0].isEmpty()) {
+            path = args[0];
+        } else {
+            path = "src/test/resources/input.txt";
+        }
+
+        Lawn lawn = new Lawn(path);
         lawn.start();
+
+        lawn.getMowers().forEach(System.out::println);
     }
 }
